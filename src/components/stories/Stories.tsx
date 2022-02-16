a6de3058147fd8ab7335f37d7ad0f81286d773b5
Michell Aagaard Dranig <tiamisse@gmail.com>
2022-02-16 14:50:17 +0100

    minor changes to styling

--- src/components/stories/Stories.tsx
+++ src/components/stories/Stories.tsx
@@ -7,13 +7,25 @@ import { getStoryIds } from '../../services/storyApi';
 import { Story } from '../story/Story';
 
 export const Stories = () => {
+  const [loading, setLoading] = useState(true);
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
-    getStoryIds().then(data => setStoryIds(data));
+    getStoryIds().then(data => {
+      setStoryIds(data);
+      setLoading(false);
+    });
   }, []);
 
   const stories = storyIds.map((storyId, key) => <Story key={key} storyId={storyId} />);
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading stories...</h1>
+      </div>
+    );
+  }
 
   return <div>{stories}</div>;
 };

fc536a414a93012cc2b79ca0e9e69cdddc01ff90
Michell Aagaard Dranig <tiamisse@gmail.com>
2022-02-16 01:37:17 +0100

    minor cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Header } from './common/header/Header';
-import { Story, StoryItem } from './components/story/Story';
 import { Stories } from './components/stories/Stories';
 
 export const App = () => (
